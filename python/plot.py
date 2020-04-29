a3f4b2d4702126b8f9f3054c8b8bc0ff202beca5
apparecidoo <39217096+apparecidoo@users.noreply.github.com>
2020-04-29 20:22:01 -0300

    Update plot.py

--- python/plot.py
+++ python/plot.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as mplt
 import matplotlib.patches as mpatches
+from matplotlib.colors import ListedColormap
 import numpy as np
 from mpl_toolkits import mplot3d
+
+def LinearRegressionIris2D(y_test, y_pred, title = 'Linear Regression of IRIS dataset'):
+    mplt.figure()
+    mplt.title(title)
+    mplt.scatter(y_test, y_pred, marker = 'o', c = y_test, edgecolors = 'black')
+    mplt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], lw=1, c = 'black')
+    mplt.show()
+
 
 def PcaIris2D(X_r, y, target_names, title = 'PCA of IRIS dataset'):
     mplt.figure()
@@ -15,7 +24,7 @@ def PcaIris2D(X_r, y, target_names, title = 'PCA of IRIS dataset'):
     mplt.title(title)
     mplt.show()
 
-def LdaIris2D(X_lda, y, label_dict, title = 'Plot Lda'):
+def Iris2D(X_lda, y, label_dict, title = 'Plot Lda'):
     ax = mplt.subplot(111)
     for label,marker,color in zip(
         range(0,3),('^', 's', 'o'),('blue', 'red', 'green')):
@@ -47,3 +56,19 @@ def LdaIris2D(X_lda, y, label_dict, title = 'Plot Lda'):
     mplt.grid()
     mplt.tight_layout
     mplt.show()
+
+def SvmIris2D(x, y, features, names, xp_svm, yp_svm, pred_svm, title = 'SVM of IRIS dataset'):
+    mplt.figure()
+    colors = ('lightblue', 'orange', 'yellow')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    mplt.contourf(xp_svm, yp_svm, pred_svm, alpha = 0.3, cmap=cmap)
+
+    for color, i, target_name in zip(colors, [0, 1, 2], names):
+        mplt.scatter(x[y == i, 0], x[y == i, 1],  color=color, label=target_name, edgecolors='black')
+
+    mplt.xlabel(features[2])
+    mplt.ylabel(features[3])
+
+    mplt.legend(loc='best', shadow=False, scatterpoints=1)
+    mplt.title(title)
+    mplt.show()    
